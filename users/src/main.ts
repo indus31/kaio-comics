17a83b78cbf117ec13b073c8fc63164566dcf298
Remi <remi.salvan@poe.aelion.fr>
2024-08-28 20:27:47 +0200

    setting users as a microservice in main.ts

--- users/src/main.ts
+++ users/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3100,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
