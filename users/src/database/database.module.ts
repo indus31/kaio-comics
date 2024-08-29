fbb2cbfe97340fb2dee7cca2af7f7f298354feda
Remi <remi.salvan@poe.aelion.fr>
2024-08-29 19:52:35 +0200

    creation of users.schema.ts and database.module.ts for mongodb

--- users/src/database/database.module.ts
+++ users/src/database/database.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { usersSchema } from 'src/model/users.schema';
 
-@Module({})
+const envfile = 'env/' + process.env.NEST_ENV;
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: envfile,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (_configService: ConfigService) => ({
+        uri: _configService.get<string>('DATABASE_URI'),
+        dbName: _configService.get<string>('DATABASE_NAME'),
+      }),
+    }),
+    MongooseModule.forFeature([{ name: 'Users', schema: usersSchema }]),
+  ],
+  exports: [MongooseModule],
+})
 export class DatabaseModule {}
---
+++ users/src/model/users.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Users {
+  @Prop()
+  id?: number;
+  @Prop()
+  lastname: string;
+  @Prop()
+  firstname: string;
+  @Prop()
+  gender: string;
+  @Prop()
+  emails: string[];
+  @Prop()
+  telephone: string;
+}
+export const usersSchema = SchemaFactory.createForClass(Users);
