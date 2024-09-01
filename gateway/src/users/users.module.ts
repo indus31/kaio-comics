bdf1da8509a85981deb3a567d7bb861cc093caca
Remi <remi.salvan@poe.aelion.fr>
2024-09-01 12:33:09 +0200

    registering user microservice in userModule in the gateway

---
+++ gateway/src/users/users.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USERS',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 3100,
+        },
+      },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
