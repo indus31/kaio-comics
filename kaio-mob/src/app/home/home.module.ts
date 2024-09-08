7cc4a60e281e179e4143dac87a1a9b963418fa3e
Remi <remi.salvan@poe.aelion.fr>
2024-09-08 13:35:25 +0200

    adding user-header component to declaration of home.module

--- kaio-mob/src/app/home/home.module.ts
+++ kaio-mob/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { UserHeaderComponent } from '../users/user-header.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,UserHeaderComponent]
 })
 export class HomePageModule {}
