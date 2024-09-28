5d8ab5aa81186c8b5b3fcfc01a5ecf7befd8c264
indus31 <indus31@hotmail.fr>
2024-09-28 20:27:47 +0200

    adding childrens routes in home routing

--- kaio-mob/src/app/home/home-routing.module.ts
+++ kaio-mob/src/app/home/home-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomePage,
+    canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: 'posts',
+        loadChildren: () => import('../all-posts/all-posts.module').then(m => m.AllPostsPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../user/user.module').then(m => m.UserPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/home/posts',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
   }
 ];
 
@@ -13,4 +35,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }
