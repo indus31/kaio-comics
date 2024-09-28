e00774c709f34910d593552558cf1d348d03b503
indus31 <indus31@hotmail.fr>
2024-09-28 20:31:16 +0200

    removing useless import

--- kaio-mob/src/app/home/home.module.ts
+++ kaio-mob/src/app/home/home.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-import { UserHeaderComponent } from '../users/user-header.component';
 
 
 @NgModule({
@@ -15,6 +14,6 @@ import { UserHeaderComponent } from '../users/user-header.component';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage,UserHeaderComponent]
+  declarations: [HomePage]
 })
 export class HomePageModule {}
