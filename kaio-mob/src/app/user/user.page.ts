590cb23e49dcc5243f8ec6d5611be60f3f64c15a
indus31 <indus31@hotmail.fr>
2024-11-01 13:03:32 +0100

    puting components on userPage

--- kaio-mob/src/app/user/user.page.ts
+++ kaio-mob/src/app/user/user.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../core/services/users/users.service';
+import { StorageService } from '../core/services/storage.service';
+import { UserType } from '../core/model/user.type';
+import { ModalController } from '@ionic/angular';
+import { UpdateProfilePictureComponent } from '../shared/components/update-profile-picture/update-profile-picture.component';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.page.scss'],
 })
 export class UserPage implements OnInit {
-
-  constructor() { }
+  user?: UserType;
+  constructor(private _userservice: UsersService, private _storageService: StorageService) { }
 
   ngOnInit() {
-  }
+    const userId = this._storageService.retrieve('session');
+    this._userservice.findOneBy(userId[0]).subscribe((user) => {
 
+      this.user = user;
+    });
+    console.log(userId[0])
+    console.log("data : " + JSON.stringify(this.user))
+  }
+ 
 }
