54c736d15f91d4754110cbfbd4e22b6a86188b82
indus31 <indus31@hotmail.fr>
2024-12-10 17:57:55 +0100

    implementing onInit and OnDestroy

--- kaio-mob/src/app/shared/components/users/user-name-profile/user-name-profile.component.ts
+++ kaio-mob/src/app/shared/components/users/user-name-profile/user-name-profile.component.ts
@@ -1,15 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { map, Subscription, take } from 'rxjs';
+
+import { FileUploadService } from 'src/app/core/file-upload.service';
 import { UserType } from 'src/app/core/model/user.type';
+import { StorageService } from 'src/app/core/services/storage.service';
+import { UsersService } from 'src/app/core/services/users/users.service';
 
 @Component({
   selector: 'app-user-name-profile',
   templateUrl: './user-name-profile.component.html',
   styleUrls: ['./user-name-profile.component.scss'],
 })
-export class UserNameProfileComponent  implements OnInit {
+export class UserNameProfileComponent  implements OnInit, OnDestroy {
+  
   @Input() user?: UserType
-  constructor() { }
+  private _subscription!: Subscription
 
-  ngOnInit() {}
+  constructor(private storageService: StorageService,private _userService:UsersService) { }
 
+  ngOnInit(): void {
+    console.log('Component initialized');
+    const id = this.storageService.retrieve('session')
+    this._subscription = this._userService.findOneBy(id).subscribe({
+      next:(user: UserType)=>{
+        this.user = user
+      },
+      error:(error:any)=>{console.log(error)},
+      complete:()=>{console.log(this.user?.profilePicture)}
+    })
+   
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['user']) {
+      console.log('restart component')
+      this.ngOnInit();
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Nettoyez les ressources si nécessaire
+    console.log('Component destroyed');
+    if (this._subscription) {
+      this._subscription.unsubscribe();
+    }
+  }
 }
+  
+
+
