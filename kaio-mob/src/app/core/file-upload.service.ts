089a010eb69f1c2751e233da17d6af0daa90348a
indus31 <indus31@hotmail.fr>
2024-12-10 16:21:17 +0100

    add a method to create a new document in profilePicture_db

--- kaio-mob/src/app/core/file-upload.service.ts
+++ kaio-mob/src/app/core/file-upload.service.ts
@@ -13,15 +13,24 @@ export class FileUploadService {
 
   uploadFile(file: File, extension: string, userId: string): Observable<any> {
     const formData: FormData = new FormData();
-    console.log('juste avant le append')
+    console.log('posting picture file.....')
+    if(!file){
+      console.log('file is empty')
+    }
     formData.append('file', file, file.name);
     formData.append('extension', extension);
     formData.append('userId', userId);
-
+    console.log('upload complete')
     return this.http.post(this.apiUrl+'/upload', formData);
   }
   getProfilePictureByUserId(userId: string): Observable<any> {
     console.log('sending request for image')
     return this.http.get(`${this.apiUrl}/provide/${userId}`);
+  }
+  createPicture(userId:string):Observable<any>{
+    console.log('request for creating document')
+    console.log('id received : '+ userId)
+    console.log('request on : '+`${this.apiUrl}/create/${userId}`)
+    return this.http.post(`${this.apiUrl}/create/${userId}`,{})
   }
 }
