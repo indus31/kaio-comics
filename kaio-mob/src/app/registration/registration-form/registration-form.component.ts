05c10d60cf28e119b4e45c685f34a8ef1e25aa86
indus31 <indus31@hotmail.fr>
2024-12-10 16:24:28 +0100

    inject users and fileUpload services

--- kaio-mob/src/app/registration/registration-form/registration-form.component.ts
+++ kaio-mob/src/app/registration/registration-form/registration-form.component.ts
@@ -5,8 +5,9 @@ import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 
 import { take } from 'rxjs';
+import { FileUploadService } from 'src/app/core/file-upload.service';
 import { RegistrationService } from 'src/app/core/services/registration.service';
-import { StorageService } from 'src/app/core/services/storage.service';
+import { UsersService } from 'src/app/core/services/users/users.service';
 
 @Component({
   selector: 'app-registration-form',
@@ -16,6 +17,8 @@ import { StorageService } from 'src/app/core/services/storage.service';
 export class RegistrationFormComponent  implements OnInit {
   public form: FormGroup = new FormGroup({})
   constructor(
+    private _fileUploadService : FileUploadService,
+    private _userservice: UsersService,
     private _formBuilder: FormBuilder,
     private _registration: RegistrationService,
     private _toastController: ToastController,
@@ -65,23 +68,50 @@ export class RegistrationFormComponent  implements OnInit {
               .then(() => this.form.reset())
   }
   onSubmit(): void {
-    console.log('begin submit')
+    console.log('begin submit');
     this._registration
       .registerUser(this.form.value)
       .pipe(take(1))
       .subscribe({
         next: (response: HttpResponse<any>) => {
           if (response.status === 200) {
-            console.log('just before navigate to login')
-            this._router.navigate(['login'])
+            const username = this.form.value.username;
+            this._userservice.findOneByUserName(username).subscribe({
+              next: (user: any) => {
+                const userId = user._id;
+                console.log(userId + 'just before createPicture');
+                this._fileUploadService.createPicture(userId).subscribe({
+                  next: () => {
+                    console.log('just before update user');
+                    // Mise à jour de l'utilisateur avec la nouvelle URI de la photo de profil
+                    user.profilePicture = 'http://localhost:3000/profile-picture/provide/' + userId;
+                    this._userservice.updateUser(user).subscribe({
+                      next: () => {
+                        console.log('just before navigate to login');
+                        this._router.navigate(['login']);
+                      },
+                      error: (error: any) => {
+                        console.log('Error updating user:', error);
+                      }
+                    });
+                  },
+                  error: (error: any) => {
+                    console.log('Error creating picture:', error);
+                  }
+                });
+              },
+              error: (error: any) => {
+                console.log('Error finding user:', error);
+              }
+            });
           } else {
             console.log(`ko, ${JSON.stringify(response.body)}`);
-            this.registrationToast('top',`${JSON.stringify(response.body)}`)
+            this.registrationToast('top', `${JSON.stringify(response.body)}`);
           }
         },
         error: (error: any) => {
-          console.log('ko,on est pas bon');
+          console.log('ko,on est pas bon', error);
         },
-      })
+      });
   }
 }
