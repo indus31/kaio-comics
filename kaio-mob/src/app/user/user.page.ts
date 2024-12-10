3063c509734413acf2a3e361eb9fdf97050eed69
indus31 <indus31@hotmail.fr>
2024-12-10 17:59:08 +0100

    add a subscription to users datas

--- kaio-mob/src/app/user/user.page.ts
+++ kaio-mob/src/app/user/user.page.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UsersService } from '../core/services/users/users.service';
 import { StorageService } from '../core/services/storage.service';
 import { UserType } from '../core/model/user.type';
 import { ModalController } from '@ionic/angular';
 import { UpdateProfilePictureComponent } from '../shared/components/update-profile-picture/update-profile-picture.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.page.html',
   styleUrls: ['./user.page.scss'],
 })
-export class UserPage implements OnInit {
+export class UserPage implements OnInit, OnDestroy {
   user?: UserType;
+  private _subscription!: Subscription
   constructor(private _userservice: UsersService, private _storageService: StorageService) { }
 
   ngOnInit() {
     const userId = this._storageService.retrieve('session');
-    this._userservice.findOneBy(userId[0]).subscribe((user) => {
-
-      this.user = user;
-    });
-    console.log(userId[0])
-    console.log("data : " + JSON.stringify(this.user))
+    this._subscription = this._userservice.findOneBy(userId).subscribe({
+      next:(user: UserType)=>{
+        this.user = user
+      },
+      error:(error:any)=>{},
+      complete:()=>{console.log('user informations loaded')}
+    })
+    
   }
- 
+  ngOnDestroy(): void {
+    // Nettoyez les ressources si nécessaire
+    console.log('Component destroyed');
+    if (this._subscription) {
+      this._subscription.unsubscribe();
+    }
+  }
 }
