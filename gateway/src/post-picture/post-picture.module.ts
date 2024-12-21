47f72413d2e9aa21dd1e441cab60e6188f43aa9f
indus31 <indus31@hotmail.fr>
2024-12-21 15:05:28 +0100

    register post_picture microservice

--- gateway/src/post-picture/post-picture.module.ts
+++ gateway/src/post-picture/post-picture.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { PostPictureService } from './post-picture.service';
 import { PostPictureController } from './post-picture.controller';
 
 @Module({
-  controllers: [PostPictureController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'POST_PICTURE_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 3002,
+        },
+      },
+    ]),
+  ],
   providers: [PostPictureService],
+  controllers: [PostPictureController],
 })
 export class PostPictureModule {}
