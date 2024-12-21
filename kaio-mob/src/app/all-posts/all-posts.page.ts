42a151e507cbba9e0540506aa66f0ada2620bb52
indus31 <indus31@hotmail.fr>
2024-12-21 13:02:07 +0100

    update post-list with new component

--- kaio-mob/src/app/all-posts/all-posts.page.ts
+++ kaio-mob/src/app/all-posts/all-posts.page.ts
@@ -8,38 +8,6 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./all-posts.page.scss'],
 })
 export class AllPostsPage {
-  constructor(
-    private _logOutService: LogoutService,
-    private alertController: AlertController
-  ) {}
-  // disconnect() {
-  //   this._logOutService.disconnect();
-  // }
-  // async presentAlert() {
-  //   const alert = await this.alertController.create({
-  //     header:
-  //       'Etes-vous sur de vouloir quitter cette application de ouf malade !',
-  //     subHeader: "t'es dingue?",
-  //     message: 'se deconnecter ?',
-  //     buttons: this.buttons,
-  //   });
-
-  //   await alert.present();
-  // }
-  // public buttons = [
-  //   {
-  //     text: 'non',
-  //     role: 'cancel',
-  //     handler: () => {
-  //       console.log('il reste !!');
-  //     },
-  //   },
-  //   {
-  //     text: 'oui',
-  //     role: 'confirm',
-  //     handler: () => {
-  //       this.disconnect();
-  //     },
-  //   },
-  // ];
+  constructor()
+   {}
 }
