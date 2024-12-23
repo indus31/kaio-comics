792d573b61e473a42e210df0e40eae231c4485b1
indus31 <indus31@hotmail.fr>
2024-12-23 15:15:22 +0100

    add methods to trigger button in all-posts

--- kaio-mob/src/app/all-posts/all-posts.page.ts
+++ kaio-mob/src/app/all-posts/all-posts.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LogoutService } from '../core/services/log-out.service';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
+import { AddPostComponent } from './components/add-post/add-post.component';
 
 @Component({
   selector: 'app-all-posts',
@@ -8,6 +9,20 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./all-posts.page.scss'],
 })
 export class AllPostsPage {
-  constructor()
-   {}
+  constructor(private _modalCtrl: ModalController, private _modalController: ModalController) { }
+  ngOnInit() {}
+  async dismiss() {
+    await this._modalController.dismiss();
+  }
+
+  async showAddPost() {
+    const newModalId = 'addPost';
+    const modal = await this._modalCtrl.create({
+      component: AddPostComponent,
+      id: newModalId,
+      
+    });
+    
+    modal.present();
+  }
 }
